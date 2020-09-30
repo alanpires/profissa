b107a75e8153f20f2c5388fd76f30a4475346d0d
Yuri Caetano <yuuri.caetano@gmail.com>
2020-09-30 15:41:20 -0300

    feat: add the content in middle of page, with svg, input search, and bottom to search in form

--- src/components/homepage/style.js
+++ src/components/homepage/style.js
@@ -2,11 +2,6 @@ import styled from "styled-components";
 
 export const ContainerFlexHomePage = styled.div`
   display: flex;
-  height: 7%;
-`;
-
-export const HeaderhomePage = styled.header`
-  width: 100%;
 `;
 
 export const LogoH1homepage = styled.h1`
@@ -14,19 +9,30 @@ export const LogoH1homepage = styled.h1`
   margin-top: 22px;
   left: 2%;
   color: black;
+  @media (max-width: 650) and (min-width: 650px) {
+  }
 `;
 
 export const DivButtonsContainer = styled.div`
   position: absolute;
   right: 20%;
   top: 23px;
-  #botton-sejaum-profissa {
-    padding-left: 50px;
-    padding-right: 50px;
+`;
+
+export const ButtonhomePage1 = styled.button`
+  border-radius: 38px;
+  border: none;
+  padding: 10px 15px 10px 15px;
+  margin-left: 10px;
+  transition: 0.3s;
+  padding-left: 50px;
+  padding-right: 50px;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.22);
   }
 `;
 
-export const ButtonhomePage = styled.button`
+export const ButtonhomePage2 = styled.button`
   border-radius: 38px;
   border: none;
   padding: 10px 15px 10px 15px;
@@ -34,6 +40,54 @@ export const ButtonhomePage = styled.button`
   transition: 0.3s;
   &:hover {
     background-color: rgba(0, 0, 0, 0.22);
+  }
+  @media (max-width: 850px) and (min-width: 650px) {
+    margin-right: 50px;
+  }
+  @media (max-width: 650) and (min-width: 650px) {
+    margin-right: 50px;
+  }
+`;
+
+export const ButtonhomePage3 = styled.button`
+  border-radius: 38px;
+  border: none;
+  padding: 10px 15px 10px 15px;
+  margin-left: 10px;
+  transition: 0.3s;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.22);
+  }
+  @media (max-width: 850px) and (min-width: 650px) {
+    position: absolute;
+    top: 50px;
+    left: 50px;
+  }
+  @media (max-width: 650) and (min-width: 650px) {
+    position: absolute;
+    top: 50px;
+    left: 50px;
+  }
+`;
+
+export const ButtonhomePage4 = styled.button`
+  border-radius: 38px;
+  border: none;
+  padding: 10px 15px 10px 15px;
+  margin-left: 10px;
+  transition: 0.3s;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.22);
+  }
+  @media (max-width: 850px) and (min-width: 650px) {
+    position: absolute;
+    top: 50px;
+    right: 90px;
+  }
+  @media (max-width: 650px) and (min-width: 650px) {
+    position: absolute;
+    top: 50px;
+    right: 90px;
   }
 `;
 
@@ -59,4 +113,66 @@ export const ImgProfileHeaderTopHomepage = styled.img`
   border-radius: 50%;
   right: 10px;
   top: 7px;
+`;
+
+export const DivContentHomepage = styled.div`
+  position: absolute;
+  top: 30vh;
+  left: 7vh;
+
+  h1 {
+    margin-bottom: -10px;
+    align-items: center;
+    font-family: Shrikhand;
+    font-size: 48px;
+  }
+`;
+
+export const ButtomsearchHomepage = styled.button`
+  border-radius: 38px;
+  border: 1px solid grey;
+  padding: 15px 10px 15px 10px;
+  transition: 0.3s;
+  margin-left: 80px;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.22);
+  }
+`;
+
+export const DivHandleInputContent = styled.div`
+  display: flex;
+  justify-content: center;
+  top: 150px;
+  margin-top: 60px;
+  border-radius: 18px;
+`;
+
+export const ImgHero = styled.div`
+  position: absolute;
+  display: flex;
+  width: 550px;
+  z-index: -1;
+  right: 50px;
+  top: 150px;
+`;
+
+export const SpanSubmitHomepage1 = styled.span`
+  display: inline-block;
+  padding: 10px;
+  background-color: #eaeaea;
+  padding: 20px;
+  input {
+    background-color: #eaeaea;
+    border: none;
+  }
+`;
+
+export const SpanSubmitHomepage2 = styled.span`
+  display: inline-block;
+  padding: 20px;
+  background-color: #eaeaea;
+  input {
+    background-color: #eaeaea;
+    border: none;
+  }
 `;
