647b1d25d9a448724cb5d7b094e2b362838f8289
Alan Cesar Pires <alancpires01@gmail.com>
2020-09-30 18:05:42 -0300

    fix: merge conflits

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from "react";
-<<<<<<< HEAD
 import Homepage from "../src/components/homepage";
-=======
 import Authentication from "./components/authentication";
 import LoginModal from "./components/login-modal";
->>>>>>> test
 
 const App = () => {
   return (
     <div>
-<<<<<<< HEAD
       <Homepage />
-=======
       <Authentication />
       <LoginModal />
->>>>>>> test
     </div>
   );
 };
