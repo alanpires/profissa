d973cec9237ef050d09e600b8ce910935fd83cab
Lucas Duarte <lucasduartedsvp@gmail.com>
2020-10-01 17:54:04 +0000

    add: style to input

--- src/components/form/style.js
+++ src/components/form/style.js
@@ -1,4 +1,12 @@
 import styled from "styled-components";
+import {Input} from 'antd'
+
+export const NewInput = styled(Input)`
+height: 200px;
+width: 400px;
+border-radius: 15px;
+margin-bottom: 20px;
+`
 
 export const FormDiv = styled.div`
   display: flex;
