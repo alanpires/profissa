cbf4582d304690b04d12bc8ccea9ac383566ec71
Alan Cesar Pires <alancpires01@gmail.com>
2020-10-01 19:43:03 -0300

    fix: fix merge conflits

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Authentication from "./components/authentication";
 import Homepage from "./pages/homepage";
+import LoginModal from "./components/login-modal";
 
 const App = () => {
   return (
     <div>
       {/* <Authentication /> */}
       <Homepage />
+      <LoginModal />
     </div>
   );
 };
