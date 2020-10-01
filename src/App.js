e532abd12bc96d42c3ab0165e9e474e4c3982932
Alan Cesar Pires <alancpires01@gmail.com>
2020-10-01 10:56:04 -0300

    feat: test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from "react";
-import Homepage from "../src/components/homepage";
 import Authentication from "./components/authentication";
 import LoginModal from "./components/login-modal";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  Redirect,
-} from "react-router-dom";
-import SignUpClient from "./pages/signup-client";
 
 const App = () => {
   return (
     <div>
-      
       <Authentication />
-      
-      
-    
+      <LoginModal />
     </div>
   );
 };
