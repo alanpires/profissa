049795f4df87f11d6ea6ca2a3c9c59bf3ed5cd48
Alan Cesar Pires <alancpires01@gmail.com>
2020-10-06 11:47:44 -0300

    feat: created style.js

--- src/pages/professional-showcase/index.jsx
+++ src/pages/professional-showcase/index.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import {
+  GeneralContainer,
+  Header,
+  ProfessionalPersonalInfos,
+  ProfessionalPersonalSkills,
+  SidebarLeft,
+  SidebarLeftOne,
+  SidebarLeftTwo,
+  SidebarRight,
+} from "./style";
 
 const ProfessionalShowcase = () => {
-  return <h1>Professional Showcase</h1>;
+  return (
+    <GeneralContainer>
+      <Header></Header>
+      <ProfessionalPersonalInfos></ProfessionalPersonalInfos>
+      <ProfessionalPersonalSkills>
+        <SidebarLeft>
+          <SidebarLeftOne></SidebarLeftOne>
+          <SidebarLeftTwo></SidebarLeftTwo>
+        </SidebarLeft>
+        <SidebarRight></SidebarRight>
+      </ProfessionalPersonalSkills>
+    </GeneralContainer>
+  );
 };
 
 export default ProfessionalShowcase;
---
+++ src/pages/professional-showcase/style.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const GeneralContainer = styled.div`
+  margin: 20px;
+  border: 1px solid black;
+  height: 100%;
+`;
+
+export const Header = styled.div`
+  border: 1px solid black;
+  height: 50px;
+`;
+
+export const ProfessionalPersonalInfos = styled.div`
+  border: 1px solid black;
+  height: 100px;
+`;
+
+export const ProfessionalPersonalSkills = styled.div`
+  border: 1px solid black;
+  height: 450px;
+`;
+
+export const SidebarLeft = styled.div`
+  border: 1px solid black;
+`;
+
+export const SidebarLeftOne = styled.div``;
+
+export const SidebarLeftTwo = styled.div``;
+
+export const SidebarRight = styled.div``;
