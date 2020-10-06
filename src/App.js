3ec3b647feb8326361fc4407f58079f1e8004653
Alan Cesar Pires <alancpires01@gmail.com>
2020-10-06 17:36:15 -0300

    fix: merge conflits

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from "react";
 import Authentication from "./components/authentication";
-import LoginModal from "./components/login-modal";
-import JobsProfissa from "./components/feedbacks-profissa";
 
 const App = () => {
   return (
     <div>
-      {/* <JobsProfissa /> */}
       <Authentication />
     </div>
   );
