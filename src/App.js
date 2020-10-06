b4241390c561f18514d28af2460deed963236db7
Alan Cesar Pires <alancpires01@gmail.com>
2020-10-06 17:44:47 -0300

    fix: test page client profile

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import Authentication from "./components/authentication";
 import ProfessionalShowcase from "./pages/professional-showcase";
 import FeedbacksProfissa from "./components/feedbacks-profissa";
+import ClientProfile from "./pages/client-profile";
 
 const App = () => {
   return (
     <div>
       {/* <Authentication /> */}
-      <ProfessionalShowcase />
-      <FeedbacksProfissa />
+      {/* <ProfessionalShowcase />
+      <FeedbacksProfissa /> */}
+      <ClientProfile />
     </div>
   );
 };
