7a4e0228f6083a4c3657e172ef2b33a0427f59ae
Alan Cesar Pires <alancpires01@gmail.com>
2020-10-06 16:56:45 -0300

    feat: added responsive layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Authentication from "./components/authentication";
 import ProfessionalShowcase from "./pages/professional-showcase";
+import FeedbacksProfissa from "./components/feedbacks-profissa";
 
 const App = () => {
   return (
     <div>
       {/* <Authentication /> */}
       <ProfessionalShowcase />
+      <FeedbacksProfissa />
     </div>
   );
 };
--- src/pages/professional-showcase/index.jsx
+++ src/pages/professional-showcase/index.jsx
@@ -12,6 +12,10 @@ import {
   ProfessionalName,
   Stars,
   ButtonToHireProfessional,
+  Container,
+  ContainerButton,
+  ContainerInfos,
+  TextSidebarRight,
 } from "./style";
 import { Estrela } from "../homepage/style";
 import LisaimgSvg from "../homepage/photos/lisa.svg";
@@ -19,30 +23,67 @@ import LisaimgSvg from "../homepage/photos/lisa.svg";
 const ProfessionalShowcase = () => {
   return (
     <>
-      <Header></Header>
+      <Header>Header Fixo</Header>
       <GeneralContainer>
         <ProfessionalPersonalInfos>
-          <Image>
-            <img src={LisaimgSvg} />
-          </Image>
-          <ProfessionalName>
-            <p>Lisa Sanchez</p>
-          </ProfessionalName>
-          <Stars>
-            <span>
-              5.0 <Estrela /> 12 avaliações
-            </span>
-          </Stars>
-          <ButtonToHireProfessional>
-            <button></button>
-          </ButtonToHireProfessional>
+          <ContainerInfos>
+            <Image src={LisaimgSvg} />
+            <Container>
+              <ProfessionalName>Lisa Sanchez</ProfessionalName>
+              <Stars>
+                5.0 <Estrela /> 12 avaliações
+              </Stars>
+            </Container>
+          </ContainerInfos>
+          <ContainerButton>
+            <ButtonToHireProfessional>
+              Contratar profissional
+            </ButtonToHireProfessional>
+          </ContainerButton>
         </ProfessionalPersonalInfos>
         <ProfessionalPersonalSkills>
           <SidebarLeft>
-            <SidebarLeftOne></SidebarLeftOne>
-            <SidebarLeftTwo></SidebarLeftTwo>
+            <SidebarLeftOne>
+              <p>Habilidades:</p>
+              <div>Lorem Ipsum</div>
+              <div>Lorem Ipsum</div>
+              <div>Lorem Ipsum</div>
+              <div>Lorem Ipsum</div>
+              <div>Lorem Ipsum</div>
+              <div>Lorem Ipsum</div>
+            </SidebarLeftOne>
+            <SidebarLeftTwo>
+              <p>Outro texto</p>
+              <div>Lorem Ipsum</div>
+              <div>Lorem Ipsum</div>
+              <div>Lorem Ipsum</div>
+              <div>Lorem Ipsum</div>
+              <div>Lorem Ipsum</div>
+              <div>Lorem Ipsum</div>
+            </SidebarLeftTwo>
           </SidebarLeft>
-          <SidebarRight></SidebarRight>
+          <SidebarRight>
+            <TextSidebarRight>
+              <p>Sobre o profissional</p>
+              <div>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+                vel quam sed augue finibus malesuada vitae at quam. Morbi at
+                tellus molestie, condimentum massa vitae, condimentum nisi.
+                Morbi ullamcorper, quam a vestibulum vulputate, augue felis
+                fringilla quam, eu scelerisque lectus ipsum at sem.
+              </div>
+              <div>
+                Aliquam egestas nisl eu magna dignissim facilisis. Vivamus
+                pulvinar risus tellus, nec congue ex convallis vitae. Mauris
+                imperdiet aliquam sagittis. Sed molestie ornare nulla, ut
+                hendrerit orci ullamcorper in. Mauris iaculis fermentum posuere.
+                Quisque vestibulum lacus sit amet est imperdiet eleifend.
+                Suspendisse pulvinar finibus eleifend. Vestibulum et suscipit
+                metus. Suspendisse et velit tempor, semper nisl ac, tempor nunc.
+                In eleifend semper diam in vehicula.
+              </div>
+            </TextSidebarRight>
+          </SidebarRight>
         </ProfessionalPersonalSkills>
       </GeneralContainer>
     </>
