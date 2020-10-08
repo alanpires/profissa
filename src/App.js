437401915bc5e5e691ff5cacb605a66ac9e898fe
Alan Cesar Pires <alancpires01@gmail.com>
2020-10-08 17:14:42 -0300

    fix: remove component of the App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Authentication from "./components/authentication";
-import ProfessionalShowcase from "./pages/professional-showcase";
 
 const App = () => {
   return (
     <div>
       <Authentication />
-      {/* <ProfessionalShowcase /> */}
     </div>
   );
 };
