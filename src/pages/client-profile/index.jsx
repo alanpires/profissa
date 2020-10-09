1a9f947dffd1acded73a568d4d4ed7b60d1d34e0
Francielle de Abreu <francielledeabreus@gmail.com>
2020-10-08 22:22:53 -0300

    feat: fix layou in /client-profile/servicos-solicitados and code refactored

--- src/pages/client-profile/index.jsx
+++ src/pages/client-profile/index.jsx
@@ -9,26 +9,20 @@ import { useSelector, useDispatch } from "react-redux";
 import { serviceRequest } from "../../redux/actions/service-request";
 import { requestProfissasHomepage } from "../../redux/actions/profissas-homepage";
 import { requestFeedbacks } from "../../redux/actions/feedbacks-request";
-import { sortProfissas } from "../homepage/helper";
+import { servicesRequestClienteCurrent, axiosConfig } from "./helper";
 
 const ClientProfile = () => {
   const token = useSelector((state) => state.access.token);
   const user = useSelector((state) => state.access.user);
-  const dispatch = useDispatch();
   const servicesRequest = useSelector((state) => state.Services.services);
+  const dispatch = useDispatch();
   const feedbacks = useSelector(
     (state) => state.ProfissaFeedbacks.feedbacksRequest
   );
   const profissas = useSelector(
     (state) => state.ProfissaHomepage.profissasRequest
   );
-  const axiosConfig = (token) => ({
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
 
-  console.log(feedbacks, profissas);
   useEffect(() => {
     dispatch(serviceRequest(axiosConfig(token)));
     dispatch(requestProfissasHomepage());
@@ -36,26 +30,6 @@ const ClientProfile = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const servicesRequestClienteCurrent = (array, user) => {
-    const profissasArray = sortProfissas(feedbacks, profissas);
-    const servicesRequestClient = [];
-    const scheduledProfissas = [];
-    array.map((serviceCurrent) => {
-      if (serviceCurrent.clienteId === user.id)
-        servicesRequestClient.push(serviceCurrent.profissa);
-    });
-    console.log(profissasArray, servicesRequestClient);
-    servicesRequestClient.map((scheduledProfissa) => {
-      profissasArray.map((profissa) => {
-        if (profissa.id === scheduledProfissa.id) {
-          scheduledProfissas.push(profissa);
-        }
-      });
-    });
-    return scheduledProfissas;
-  };
-
-  console.log(servicesRequestClienteCurrent(servicesRequest, user));
   return (
     <>
       <MainDiv>
@@ -67,25 +41,28 @@ const ClientProfile = () => {
           <ProfileTitle>Serviços solicitados</ProfileTitle>
           <DivCard>
             {profissas && feedbacks ? (
-              servicesRequestClienteCurrent(servicesRequest, user).map(
-                (user, index) => (
-                  <Card className="card" key={index}>
-                    <InfoCard>
-                      <img src={RicardoSvg} />
-                      <div>
-                        {Array.from({ length: user.stars }, (v, k) => k).map(
-                          (key) => {
-                            return <EstrelaCards key={key} />;
-                          }
-                        )}
-                      </div>
-                      <p>{user.avaliations} avaliações</p>
-                      <h1>{user.name}</h1>
-                      <h1>{user.service}</h1>
-                    </InfoCard>
-                  </Card>
-                )
-              )
+              servicesRequestClienteCurrent(
+                servicesRequest,
+                user,
+                feedbacks,
+                profissas
+              ).map((user, index) => (
+                <Card className="card" key={index}>
+                  <InfoCard>
+                    <img src={RicardoSvg} />
+                    <div>
+                      {Array.from({ length: user.stars }, (v, k) => k).map(
+                        (key) => {
+                          return <EstrelaCards key={key} />;
+                        }
+                      )}
+                    </div>
+                    <p>{user.avaliations} avaliações</p>
+                    <h1>{user.name}</h1>
+                    <h1>{user.service}</h1>
+                  </InfoCard>
+                </Card>
+              ))
             ) : (
               <h1>Carregando</h1>
             )}
