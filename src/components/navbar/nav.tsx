d2f16d4ca93ed6540924bef8b6002eb8e68152cf
Yuri Caetano <yuuri.caetano@gmail.com>
2020-10-14 17:36:59 -0300

    feat: typescript in nav bar, test in progress

--- src/components/navbar/nav.tsx
+++ src/components/navbar/nav.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import Burguer from './Burguer.jsx'
 import { StyledNav } from './style'
 
-/*
-interface {
+interface Props {
   open: Boolean;
   showLogin: () => void;
-}*/
+  setShowLogin: () => void;
+}
 
-const Nav = ({ open, showLogin, setShowLogin }) => {
+const Nav = ({ open, showLogin, setShowLogin } : Props) => {
     return (
     <>
         <StyledNav>
